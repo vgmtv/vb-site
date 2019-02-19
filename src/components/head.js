59a249f65ae2a801ee48be92abbb5a3689307f3b
nampdn <nampdn@protonmail.com>
2019-02-19 09:56:56 +0700

    feat(head): add meta description

--- src/components/head.js
+++ src/components/head.js
@@ -4,6 +4,7 @@ import {Helmet} from 'react-helmet';
 export default () => (
   <Helmet>
     <title>VietBible - Ứng dụng nghiên cứu Kinh Thánh cho người Việt</title>
+    <meta type="description" content="Ứng dụng nghiên cứu Kinh Thánh tiếng Việt" />
     <link rel="icon" type="image/gif" href="./vietbible.png" />
     <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet" />
   </Helmet>
