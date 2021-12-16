6b971044983dd4ac2ab5121f48adc3b02bc30f50
Nam Pham <nampdn@protonmail.com>
2021-12-16 10:13:50 +0700

    Update head.js

--- src/components/head.js
+++ src/components/head.js
@@ -3,8 +3,8 @@ import {Helmet} from 'react-helmet';
 
 export default () => (
   <Helmet>
-    <title>VietBible - Ứng dụng nghiên cứu Kinh Thánh cho người Việt</title>
-    <meta type="description" content="Ứng dụng nghiên cứu Kinh Thánh tiếng Việt" />
+    <title>Viet.Bible - Ứng dụng tra cứu Kinh Thánh nguyên ngữ cho người Việt</title>
+    <meta type="description" content="Ứng dụng tra cứu Kinh Thánh nguyên ngữ cho người Việt" />
     <link rel="icon" type="image/gif" href="./vietbible.png" />
     <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet" />
   </Helmet>
